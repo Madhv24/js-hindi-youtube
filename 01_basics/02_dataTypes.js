6dee2dd965470cd38a9c0c5e3a2031565a2629b9
Madhv24 <152499519+Madhv24@users.noreply.github.com>
2025-08-08 06:34:33 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node.js , not browser
+
+let age = 18
+let isLoggedIn = false
+let state = null
+let location;
+console.log(typeof age);
+console.log(typeof state);
+console.log(typeof location);
