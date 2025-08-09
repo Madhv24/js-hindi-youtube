6fe65d7738c7d76d4a903ea569c8539f199c78f1
Madhv24 <152499519+Madhv24@users.noreply.github.com>
2025-08-09 18:55:42 +0000

    comparision operator

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,15 @@
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null>0);
+// console.log(null==0);  // 0 assign in null
+// console.log(null>=0);
+
+// console.log(undefined == 0);
+// console.log(undefined>0);
+// console.log(undefined<0);
+
+// ===
+
+console.log("2"===2);  // check data type is same or not
+console.log(2===2);
