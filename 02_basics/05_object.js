995fb591b9a958a06e75d085ac09cf279ac3f4c6
Madhv24 <152499519+Madhv24@users.noreply.github.com>
2025-08-14 19:40:51 +0000

    jason

---
+++ 02_basics/05_object.js
@@ -0,0 +1,25 @@
+const course = {
+    coursename:"Js in hindi",
+    price:999,
+    courseInstructor:"Hitesh"
+}
+// const{courseInstructor} = course
+//or
+const{courseInstructor:instructor} = course
+console.log(instructor);
+
+//JASON format
+
+// {
+//     "name":"hitesh",
+//     "coursename":"Js in hindi",
+//     "prics":"free"
+// }
+
+//or
+
+[
+    {},
+    {},
+    {}
+]
